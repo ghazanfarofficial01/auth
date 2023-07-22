3ab402191915dec0c3f2d46734607e7b0d58e40f
ghazanfarofficial01 <ghazanfar.official@outlook.com>
2023-07-22 23:22:12 +0530

    same as previous

--- routes/getMobiles.js
+++ routes/getMobiles.js
@@ -3,7 +3,7 @@ const MobilesRouter = express.Router();
 const Mobile = require('../models/mobile')
 
 //to fetch best condtion mobiles
-MobilesRouter.get('/api/get-mobiles-best-conditions', async (req, res) => {
+MobilesRouter.get('/api/get-mobiles-best-condition', async (req, res) => {
     try{
     const products = await Mobile.find({condition:{$gt:4}})
     res.json(products);
@@ -11,5 +11,35 @@ MobilesRouter.get('/api/get-mobiles-best-conditions', async (req, res) => {
         res.status(500).json({ error: e.message });
       }
 })
+// to fetch gaming mobiles
+MobilesRouter.get('/api/get-mobiles-gaming', async (req, res) => {
+  try{
+  const products = await Mobile.find({category:"gaming"})
+  
+  res.json(products);
+  }catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+})
+//to fetch mobiles under RS 7000
+MobilesRouter.get('/api/get-mobiles-budget', async (req, res) => {
+  try{
+  const products = await Mobile.find({price:{$lte:7000}})
+  
+  res.json(products);
+  }catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+})
 
+//to fetch premium mobiles
+MobilesRouter.get('/api/get-mobiles-premium', async (req, res) => {
+  try{
+  const products = await Mobile.find({price:{$gte:16000}})
+  
+  res.json(products);
+  }catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+})
 module.exports = MobilesRouter;
