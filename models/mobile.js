88bee00647cd42e16bd653d52f323b573625d03e
ghazanfarofficial01 <ghazanfar.official@outlook.com>
2023-07-26 16:52:52 +0530

    changed mobile schema

--- models/mobile.js
+++ models/mobile.js
@@ -1,59 +1,124 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const mobileSchema = mongoose.Schema({
-    brand:{
+  brand: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  pictures: [
+    {
       type: String,
-      required: true,
-      trim: true,
     },
-    pictures:[
-     {
-      type: String,
-     }
-    ],
-    modelName:{
-        type: String,
-        required: true,
-        trim: true,
-    },
-    price:{
-      type: Number,
-      required: true,
-    },
-    discount_percentage:{
-      type: Number,
-      default:0
+  ],
+  modelName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
 
-    },
-    description:{
-      type: String,
-      trim: true
-    },
+  originalPrice: {
+    type: Number,
+    required: true,
+    default: 5000,
+  },
 
-    condition:{
-       type: Number,
-       required: true,
-       min:[1,'at least one star required'],
-       max:[5,'at most five starts ']
-    },
+  discount_percentage: {
+    type: Number,
+    default: 0,
+  },
+  description: {
+    type: String,
+    trim: true,
+    default: "",
+  },
 
-    rating:{
-      type: Number,
-      min:[1,'least is one'],
-      max:[5,'most is five']
-    },
-    inStock:{
-       type:Number,
-       required:true
-    },
+  condition: {
+    type: Number,
+    required: true,
+    min: [1, "at least one star required"],
+    max: [5, "at most five starts "],
+  },
 
-    category:{
-      type: String,
-      trim: true,
-    }
+  rating: {
+    type: Number,
+    min: [1, "least is one"],
+    max: [5, "most is five"],
+  },
 
-})
+  unitsSold: {
+    type: Number,
+    default: 10,
+  },
+
+  inStock: {
+    type: Number,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    trim: true,
+  },
+
+  RAM: {
+    type: String,
+    defaul: "",
+  },
+  storage: {
+    type: String,
+    default: "",
+  },
+
+  Battery: {
+    type: String,
+    default: "",
+  },
+
+  front_camera: {
+    type: String,
+    default: "",
+  },
+
+  rear_camera: {
+    type: String,
+    default: "",
+  },
+
+  display: {
+    type: String,
+    default: "",
+  },
+
+  warranty: {
+    type: Number,
+    default: 12,
+  },
+
+  color: {
+    type: String,
+    default: "Blue",
+  },
+
+  in_the_box: {
+    type: String,
+    default: "",
+  },
+});
 
 const Mobile = mongoose.model("Mobile", mobileSchema);
 module.exports = Mobile;
+
+// RAM
+// STORAGE
+// Battery
+// front camera
+// rear camera
+// display
+// warranty
+// color
+// in the box
--- routes/getMobiles.js
+++ routes/getMobiles.js
@@ -1,58 +1,56 @@
-const express = require('express')
+const express = require("express");
 const MobilesRouter = express.Router();
-const Mobile = require('../models/mobile')
-const Brands = require('../models/brands')
+const Mobile = require("../models/mobile");
+const Brands = require("../models/brands");
 //to fetch best condtion mobiles
-MobilesRouter.get('/api/get-mobiles-best-condition', async (req, res) => {
-    try{
-    const products = await Mobile.find({condition:{$gt:4}})
+MobilesRouter.get("/api/get-mobiles-best-condition", async (req, res) => {
+  try {
+    const products = await Mobile.find({ condition: { $gt: 4 } });
     res.json(products);
-    }catch (e) {
-        res.status(500).json({ error: e.message });
-      }
-})
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 // to fetch gaming mobiles
-MobilesRouter.get('/api/get-mobiles-gaming', async (req, res) => {
-  try{
-  const products = await Mobile.find({category:"gaming"})
-  
-  res.json(products);
-  }catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-})
+MobilesRouter.get("/api/get-mobiles-gaming", async (req, res) => {
+  try {
+    const products = await Mobile.find({ category: "gaming" });
+
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 //to fetch mobiles under RS 7000
-MobilesRouter.get('/api/get-mobiles-budget', async (req, res) => {
-  try{
-  const products = await Mobile.find({price:{$lte:7000}})
-  
-  res.json(products);
-  }catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-})
+MobilesRouter.get("/api/get-mobiles-budget", async (req, res) => {
+  try {
+    const products = await Mobile.find({ price: { $lte: 7000 } });
+
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 //to fetch premium mobiles
-MobilesRouter.get('/api/get-mobiles-premium', async (req, res) => {
-  try{
-  const products = await Mobile.find({price:{$gte:16000}})
-  
-  res.json(products);
-  }catch (e) {
-      res.status(500).json({ error: e.message });
-    }
-})
+MobilesRouter.get("/api/get-mobiles-premium", async (req, res) => {
+  try {
+    const products = await Mobile.find({ price: { $gte: 16000 } });
+
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
 
 //to fetch mobile brands
-MobilesRouter.get('/api/brands', async(req,res)=>{
-   try{
-      const brands = await Brands.find({});
-      res.json(brands)
-   }catch(e){
+MobilesRouter.get("/api/brands", async (req, res) => {
+  try {
+    const brands = await Brands.find({});
+    res.json(brands);
+  } catch (e) {
     res.status(500).json({ error: e.message });
-   }
-
-})
-
+  }
+});
 
 module.exports = MobilesRouter;
